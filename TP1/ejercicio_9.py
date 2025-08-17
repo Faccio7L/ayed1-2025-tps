ef3ee59e304013b3b7d1b4214143f38080274dd4
Faccio7L <Santifaccio123@gmail.com>
2025-08-17 20:58:12 -0300

    Ejercicio 9 Tp1

---
+++ TP1/ejercicio_9.py
@@ -0,0 +1,44 @@
+import random as rn
+
+def cant_camiones(peso):
+    capacidad_max = 500000 # esto esta en gramos como el peso de las naranjas!!! 
+    capacidad_min = 400000 
+    camiones = peso // capacidad_max
+    sobrante = peso % capacidad_max #para ver si usamos un camion mas.
+    if sobrante >= capacidad_min: 
+        camiones += 1
+    return camiones
+
+
+def cajones_naranjas(naranjas):
+    naranjas_para_jugo = 0
+    peso_naranja_total = 0
+    cant_naranjas = 0
+    cant_cajones = 0
+    for x in range(naranjas):
+        peso_naranja = rn.randint(150,350)
+        if peso_naranja >= 200 and peso_naranja <= 300:
+            cant_naranjas += 1
+            peso_naranja_total += peso_naranja
+            if cant_naranjas == 100:
+                cant_naranjas = 0
+                cant_cajones += 1
+        else:
+            naranjas_para_jugo += 1
+    resto = cant_naranjas % 100 #lo que sobra de las naranjas.
+    camiones = cant_camiones(peso_naranja_total)
+    cajones = cant_cajones
+    return cajones, camiones, resto, naranjas_para_jugo
+            
+def validar_naranjas(naranjas):
+    return naranjas >= 1
+
+
+def main():
+    cant_naranjas = int(input("Ingrese la cantidad de naranjas:"))
+    if validar_naranjas(cant_naranjas):
+        calculos = cajones_naranjas(cant_naranjas)
+    print(f"Sobran un total de {calculos[2]} naranjas, se utilizan un total de {calculos[1]} camiones , se llenaron un total de {calculos[0]} cajones y {calculos[3]} son naranjas para jugo. ")
+        
+
+main()
