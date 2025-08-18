f5d92be54a3bfbbb0a501820a4eba653b52cee7f
Faccio7L <Santifaccio123@gmail.com>
2025-08-18 00:16:07 -0300

    Agregado de contratos

--- TP1/ejercicio_1.py
+++ TP1/ejercicio_1.py
@@ -1,4 +1,9 @@
-def num_mayor(a = 5, b = 7, c = 12): #valores default para testear.
+def num_mayor(a:int,b:int, c:int) ->int: 
+    """
+    Evalua cual es el numero mayor y si se encuentra repetido.
+    pre: Recibe 3 numeros enteros.
+    post: Devuelve el numero mayor o, en caso de que el mismo se encuentre repetido, printea -1.
+    """
     lista = [a,b,c]
     mayor = max(lista) #designo el valor mayor y lo almaceno aca.
     if lista.count(mayor) > 1: #cuento cuantas veces aparece el valor mayor.
@@ -7,7 +12,12 @@ def num_mayor(a = 5, b = 7, c = 12): #valores default para testear.
         print(mayor)
 
 
-def validar_valores(a,b,c):
+def validar_valores(a:int,b:int,c:int) ->  bool:
+    """""
+    Valida que los numeros ingresados sean enteros
+    pre:Recibe 3 numeros enteros.
+    Post: retorna True en caso de que sean los 3 positivos, caso contrario retorna false.
+    """""
     lista = [a,b,c]
     for x in lista:
         if x <= 0:
@@ -18,7 +28,12 @@ def validar_valores(a,b,c):
     #return a > 0 and b > 0 and c > 0 #retorna directamente True o False, como el ejemplo
                                       #de verificar horas (clase 1).
     
-def main():
+def main()->None:
+    """
+    Se ingresan 3 valores numericos
+    pre:No recibe parametros
+    Post: Luego de ser validados, invoca la funcion num_mayor.
+    """
     print("A continuacion, ingrese 3 numeros mayores a 0.")
     num_1 = int(input("Ingrese el primer numero:"))
     num_2 = int(input("Ingrese el segundo numero:"))
