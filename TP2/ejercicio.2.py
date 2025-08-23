2639853716fdb1c6e0ca080af2b4ea83852456b5
Faccio7L <Santifaccio123@gmail.com>
2025-08-23 19:42:38 -0300

    ejercicio 1 y 2 tp 2

---
+++ TP2/ejercicio.2.py
@@ -0,0 +1,57 @@
+import random as rn
+
+def lista_4_digitos(cant_numeros:int,lista:list)-> list[int]:
+    """
+    Completa con la cantidad de numeros elegidos por el usuario una lista    
+    pre: Recibe como parametro la lista vacia y la cantidad de numeros a appendear.
+    post: Retorna la lista llena con numeros aleatorios del 1 al 100
+    """
+
+    for x in range(cant_numeros):
+        lista.append(rn.randint(1,100))
+    return lista
+
+def verificar_elementos_repetidos(lista:list[int])-> bool:   
+    """
+    Verifica si hay numeros repetidos.
+    pre: Recibe como parametro una lista de enteros.
+    post: Retorna True en caso de que haya repetidos, caso contrario retorna False.
+    """
+    for x in lista:
+        contar_cant = lista.count(x)
+        if contar_cant >= 2:
+            return True
+    else:
+        return False
+
+def lista_no_repetidos(lista:list[int]) -> list[int]:
+    """
+    Toma una lista de enteros y genera una nueva sin repetidos
+    pre: toma como parametros una lista de enteros.
+    post: retorna una lista de numeros SIN repetir.
+    """
+    lista_sin_repetir = []
+    for elemento in lista:
+        contar_cant = lista.count(elemento)
+        if contar_cant == 1:
+            lista_sin_repetir.append(elemento)
+    return lista_sin_repetir
+
+
+
+
+def main()->None:
+    """
+    Genera una lista vacia, invoca al resto de funciones.
+    pre: No recibe parametros.
+    post: Printea los valores retornados por el resto de funciones.
+    """
+    lista = []
+    cantidad_de_numeros = int(input("Ingrese cuantos numeros desea en la lista:"))
+    print(f"La lista generada es:{lista_4_digitos(cantidad_de_numeros,lista)}")
+    if verificar_elementos_repetidos(lista):
+        print("Hay elementos repetidos.")
+    else:
+        print("NO hay elementos repetidos.")
+    print(f"la lista sin repetidos es {lista_no_repetidos(lista)}")
+main()
---
+++ TP2/ejercicio1.py
@@ -0,0 +1,62 @@
+import random as rn
+def cuatro_numeros_random(lista:list) ->list[int]:
+    """
+    recibe una lista vacia y appendea una con 4 numeros random de 1000 a 9999
+    pre: Recibe una lista vacia
+    post: Retorna una lista con 4 numero.
+    """
+    for x in range(4):
+        lista.append(rn.randint(1000,9999))
+    return lista
+
+
+def eliminar_de_La_lista(lista:list[int]) -> bool:
+    """
+    Elimina un numero de una lista en caso de que exista.
+    pre: recibe la lista de enteros
+    post: Retorna la lista con el numero eliminado.
+    """
+    numero_a_eliminar = int(input("Ingrese un numero que desea eliminar:"))
+    if numero_a_eliminar in lista:
+        lista.remove(numero_a_eliminar)
+    return lista
+    
+def lista_capicua(lista:list[int]) -> bool:
+    """
+    Verifica si una lista es capicua.
+    pre: Recibe como parametro una lista.
+    post: Retorna True o False, True en caso de que sea capicua.
+    """
+    return lista == lista[::-1]
+
+def calcular_producto(lista:list[int])-> int: #interpreto que la consigna se refiere a esto.
+    """
+    Calcula el producto de una lista(funciona con cualquier iterable)
+    pre:Recibe como parametro una lista de enteros.
+    post: retorna el producto (entero) de la lista.
+    """
+    producto = 1
+    for numero in lista:
+        producto *= numero
+    return producto     
+    
+def main()-> None:
+    """
+    Crea una lista vacia, invoca el resto de funciones y printea su return.
+    pre: No recibe parametros.
+    post: Printea informacion sobre el resto de funciones
+    """
+    lista = []
+    print(f"La lista generada de manera aleatoria es {cuatro_numeros_random(lista)}")
+    print(f"El producto de la lista anterior es {calcular_producto(lista)}")
+    print(f"Ahora la lista es {eliminar_de_La_lista(lista)}")
+    if lista_capicua(lista):
+        print("Su lista es capicua")
+    else:
+        print("Su lista no es capicua")
+#test
+assert lista_capicua([1,3,1]) == True
+assert calcular_producto([2,5,3]) == 30
+
+
+main()
